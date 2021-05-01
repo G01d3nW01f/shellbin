fa6f957acfb23e828d4abd5c8aebe01a3d9c0209
Ki11i0n4ir3 <75846902+Ki11i0n4ir3@users.noreply.github.com>
2021-05-02 00:05:16 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+
+if os.getuid() != 0:
+  print("[!]YouMustExecute as Root!!!!!!!!!")
+  sys.exit()
+ 
+os.system("chmod +x shellbin.py")
+os.system("cp shellbin /usr/local/bin/shellbin")
+
+print("[+]SetUp Done.......")
